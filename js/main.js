0cff7101d320a150703b40c13805dad898073824
LuisDavid1998 <jimenezldm@gmail.com>
2022-07-22 13:15:59 -0500

    Complete without responsive

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@ open_rules.click(() => {
 close_rules.click(() => {
     rules.hide('fast');
 });
+//Reset button
+const reset_button = $('#reset-button');
+reset_button.click(() => {
+    score_user = 0;
+    score_pc = 0;
+    pc_number.html(`0`);
+    my_number.html(`0`);
+});
 
 //Game selection
 const selection = $('#selection');
@@ -21,15 +29,65 @@ rock.click(() => {
     selection.hide('fast');
     game.show('slow');
     game.css('display','flex');
+    result('rock'); 
 });
 paper.click(() => {
     selection.hide('fast');
     game.show('slow');
     game.css('display','flex');
+    result('paper'); 
 });
 scissor.click(() => {
     selection.hide('fast');
     game.show('slow');
     game.css('display','flex');
+    result('scissors');  
 });
 
+//Winner o Loser
+const user_select = $('#game img[alt="Selection User"]');
+const pc_select = $('#game img[alt="Selection PC"]');
+const op1 = $('#game .op1');
+const op2 = $('#game .op2');
+
+function result(opt){
+    var juego = new Game();
+    var pc_selection = juego.pcSelection();
+    var user_selection = juego.userSelection(opt);
+    user_select.attr('src', `img/icon-${opt}.svg`);
+    pc_select.attr('src',`img/icon-${pc_selection}.svg`);
+    op2.addClass(`${pc_selection}`);
+    op1.addClass(opt);
+
+    WinLose(juego.gamePlay());
+}
+
+const win_lose = $('#win-lose');
+const you = $('#win-lose h3');
+const pc_number = $('#pc-number');
+const my_number = $('#my-number');
+var score_user = 0;
+var score_pc = 0;
+
+function WinLose(gameWL){
+    win_lose.show();
+    game.css('justify-content', 'space-evenly');
+    you.html(`YOU ${gameWL}`);
+
+    if(gameWL == 'WIN'){
+        score_user++;
+    }else if(gameWL == 'LOSE'){
+        score_pc++;
+    }
+    pc_number.html(`${score_pc}`);
+    my_number.html(`${score_user}`);
+}
+
+const play_again = $('.play-again');
+play_again.click(() => {
+    win_lose.hide();
+    game.hide();
+    selection.show();
+    op1.removeClass('rock paper scissors');
+    op2.removeClass('rock paper scissors');
+});
